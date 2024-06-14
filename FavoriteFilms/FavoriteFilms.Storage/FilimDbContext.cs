e1e82a1a51c8e9a17529cd83206bc7796e4faf8d
Kerven Jumayev <kerwen.jumazew@gmail.com>
2024-06-14 10:55:07 +0300

    changed FilimDbContext class

--- FavoriteFilms/FavoriteFilms.Storage/FilimDbContext.cs
+++ FavoriteFilms/FavoriteFilms.Storage/FilimDbContext.cs
@@ -7,6 +7,11 @@ namespace FavoriteFilms.Storage
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Filim> Filims { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
 
         public FilimDbContext(DbContextOptions<FilimDbContext> options):base(options)
         {
